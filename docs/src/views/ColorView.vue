61a4cf7fe46de9e8e291cc9e2d96153b5f5d3fc2
César Rovelo <ces.rovelo@gmail.com>
2023-07-26 13:18:07 -0600

    Agregando alertas
    Se agrega HTML y estilos de alertas

--- docs/src/views/ColorView.vue
+++ docs/src/views/ColorView.vue
@@ -1,15 +1,18 @@
 <template>
-
   <div>
     <h1>Color</h1>
     <h2 class="m-t-10">Identidad Gobierno de México</h2>
     <div class="grid">
-      <div class="columna-2 m-y-1 gobmx-fondo-oscuro-1 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 gobmx-fondo-oscuro-1 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.gobmx-fondo-oscuro-1</p>
         <p>#18302D</p>
       </div>
-      <div class="columna-2 m-y-1 gobmx-fondo-oscuro-2 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 gobmx-fondo-oscuro-2 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.gobmx-fondo-oscuro-2</p>
         <p>#103e34</p>
@@ -31,23 +34,33 @@
       </div>
     </div>
     <div class="grid">
-      <div class="columna-2 m-y-1 gobmx-borde-oscuro-1 borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 gobmx-borde-oscuro-1 borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.gobmx-borde-oscuro-1</p>
       </div>
-      <div class="columna-2 m-y-1 gobmx-borde-oscuro-2 borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 gobmx-borde-oscuro-2 borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.gobmx-borde-oscuro-2</p>
       </div>
-      <div class="columna-2 m-y-1 gobmx-borde-claro-1 borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 gobmx-borde-claro-1 borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.gobmx-borde-claro-1</p>
       </div>
-      <div class="columna-2 m-y-1 gobmx-borde-claro-2 borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 gobmx-borde-claro-2 borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.gobmx-borde-claro-2</p>
       </div>
-      <div class="columna-2 m-y-1 gobmx-borde-acento borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 gobmx-borde-acento borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.gobmx-borde-acento</p>
       </div>
@@ -88,64 +101,90 @@
 
     <h2 class="m-t-10">Identidad Conahcyt</h2>
     <div class="grid">
-      <div class="columna-2 m-y-1 conahcyt-fondo-oscuro-1 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 conahcyt-fondo-oscuro-1 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.conahcyt-fondo-oscuro-1</p>
         <p>#002663</p>
       </div>
-      <div class="columna-2 m-y-1 conahcyt-fondo-oscuro-2 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 conahcyt-fondo-oscuro-2 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.conahcyt-fondo-oscuro-2</p>
         <p>#003384</p>
       </div>
-      <div class="columna-2 m-y-1 conahcyt-fondo-claro-0 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 conahcyt-fondo-claro-0 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.conahcyt-fondo-claro-0</p>
         <p>#DDDDDD4c</p>
       </div>
-      <div class="columna-2 m-y-1 conahcyt-fondo-claro-1 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 conahcyt-fondo-claro-1 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.conahcyt-fondo-claro-1</p>
         <p>#d5d5d5</p>
       </div>
-      <div class="columna-2 m-y-1 conahcyt-fondo-claro-2 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 conahcyt-fondo-claro-2 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.conahcyt-fondo-claro-2</p>
         <p>#8C8D8E</p>
       </div>
-      <div class="columna-2 m-y-1 conahcyt-fondo-acento borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 conahcyt-fondo-acento borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.conahcyt-fondo-acento</p>
         <p>#13aff0</p>
       </div>
-      <div class="columna-2 m-y-1 conahcyt-fondo-degradado borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 conahcyt-fondo-degradado borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.conahcyt-fondo-degradado</p>
         <p>#003384, #002663, #001c4a</p>
       </div>
     </div>
     <div class="grid">
-      <div class="columna-2 m-y-1 conahcyt-borde-oscuro-1  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 conahcyt-borde-oscuro-1 borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.conahcyt-borde-oscuro-1</p>
       </div>
-      <div class="columna-2 m-y-1 conahcyt-borde-oscuro-2  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 conahcyt-borde-oscuro-2 borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.conahcyt-borde-oscuro-2</p>
       </div>
-      <div class="columna-2 m-y-1 conahcyt-borde-claro-0  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 conahcyt-borde-claro-0 borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.conahcyt-borde-claro-0</p>
       </div>
-      <div class="columna-2 m-y-1 conahcyt-borde-claro-1  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 conahcyt-borde-claro-1 borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.conahcyt-borde-claro-1</p>
       </div>
-      <div class="columna-2 m-y-1 conahcyt-borde-claro-2  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 conahcyt-borde-claro-2 borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.conahcyt-borde-claro-2</p>
       </div>
-      <div class="columna-2 m-y-1 conahcyt-borde-acento  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 conahcyt-borde-acento borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.conahcyt-borde-acento</p>
       </div>
@@ -213,19 +252,27 @@
       </div>
     </div>
     <div class="grid">
-      <div class="columna-2 m-y-1 borde-informativo  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 borde-informativo borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.borde-informativo</p>
       </div>
-      <div class="columna-2 m-y-1 borde-error  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 borde-error borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.borde-error</p>
       </div>
-      <div class="columna-2 m-y-1 borde-advertencia  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 borde-advertencia borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.borde-advertencia</p>
       </div>
-      <div class="columna-2 m-y-1 borde-correcto  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 borde-correcto borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.borde-correcto</p>
       </div>
@@ -280,19 +327,27 @@
       </div>
     </div>
     <div class="grid">
-      <div class="columna-2 m-y-1 borde-informativo-2  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 borde-informativo-2 borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.borde-informativo-2</p>
       </div>
-      <div class="columna-2 m-y-1 borde-error-2  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 borde-error-2 borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.borde-error-2</p>
       </div>
-      <div class="columna-2 m-y-1 borde-advertencia-2  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 borde-advertencia-2 borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.borde-advertencia-2</p>
       </div>
-      <div class="columna-2 m-y-1 borde-correcto-2  borde borde-grosor-3 borde-redondeado-8"></div>
+      <div
+        class="columna-2 m-y-1 borde-correcto-2 borde borde-grosor-3 borde-redondeado-8"
+      ></div>
       <div class="columna-2">
         <p>.borde-correcto-2</p>
       </div>
@@ -323,6 +378,232 @@
         <p>.texto-correcto-2</p>
       </div>
     </div>
-  </div>
+    <div>
+      <section class="alertas">
+        <div class="alertas__contenedor contenedor">
+          <article class="alerta informativa">
+            <div class="alerta__info">
+              <div class="alerta__encabezado">
+                <div class="alerta__info--titulo">
+                  <svg
+                    id="information"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      id="Trazado_28876"
+                      data-name="Trazado 28876"
+                      d="M15.75,20V14h-3v1.5h1.5V20H12v1.5h6V20Z"
+                      transform="translate(-3 -3.5)"
+                      fill="#19439C"
+                    />
+                    <path
+                      id="Trazado_28877"
+                      data-name="Trazado 28877"
+                      d="M15.625,8A1.125,1.125,0,1,0,16.75,9.125,1.125,1.125,0,0,0,15.625,8Z"
+                      transform="translate(-3.625 -2)"
+                      fill="#19439C"
+                    />
+                    <path
+                      id="Trazado_28878"
+                      data-name="Trazado 28878"
+                      d="M12.5,23A10.5,10.5,0,1,1,23,12.5,10.5,10.5,0,0,1,12.5,23Zm0-19.5a9,9,0,1,0,9,9A9,9,0,0,0,12.5,3.5Z"
+                      transform="translate(-0.5 -0.5)"
+                      fill="#19439C"
+                    />
+                    <rect
+                      id="_Transparent_Rectangle_"
+                      data-name="&lt;Transparent Rectangle&gt;"
+                      width="24"
+                      height="24"
+                      fill="none"
+                    />
+                  </svg>
 
+                  <!-- <img
+                    src="/img/alerta_icono.svg"
+                    alt=""
+                    class="alerta__icono"
+                  /> -->
+
+                  <p class="alerta__titulo">Alerta informativa</p>
+                </div>
+
+                <img
+                  src="/img/cerrar_icono.svg"
+                  alt=""
+                  class="alerta__cerrar--icono"
+                />
+              </div>
+              <div class="alerta__contenido">
+                <p class="alerta__parrafo">
+                  Este es el contenido de una alerta informativa que proporciona
+                  información relevante a la persona usuaria sobre el estado del
+                  sistema
+                </p>
+              </div>
+            </div>
+          </article>
+          <article class="alerta confirmacion">
+            <div class="alerta__info">
+              <div class="alerta__encabezado">
+                <div class="alerta__info--titulo">
+                  <svg
+                    id="information"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      id="Trazado_28876"
+                      data-name="Trazado 28876"
+                      d="M15.75,20V14h-3v1.5h1.5V20H12v1.5h6V20Z"
+                      transform="translate(-3 -3.5)"
+                      fill="#2A6F4D"
+                    />
+                    <path
+                      id="Trazado_28877"
+                      data-name="Trazado 28877"
+                      d="M15.625,8A1.125,1.125,0,1,0,16.75,9.125,1.125,1.125,0,0,0,15.625,8Z"
+                      transform="translate(-3.625 -2)"
+                      fill="#2A6F4D"
+                    />
+                    <path
+                      id="Trazado_28878"
+                      data-name="Trazado 28878"
+                      d="M12.5,23A10.5,10.5,0,1,1,23,12.5,10.5,10.5,0,0,1,12.5,23Zm0-19.5a9,9,0,1,0,9,9A9,9,0,0,0,12.5,3.5Z"
+                      transform="translate(-0.5 -0.5)"
+                      fill="#2A6F4D"
+                    />
+                    <rect
+                      id="_Transparent_Rectangle_"
+                      data-name="&lt;Transparent Rectangle&gt;"
+                      width="24"
+                      height="24"
+                      fill="none"
+                    />
+                  </svg>
+
+                  <!-- <img
+                    src="/img/alerta_icono.svg"
+                    alt=""
+                    class="alerta__icono"
+                  /> -->
+
+                  <p class="alerta__titulo">Alerta confirmación</p>
+                </div>
+
+                <img
+                  src="/img/cerrar_icono.svg"
+                  alt=""
+                  class="alerta__cerrar--icono"
+                />
+              </div>
+              <div class="alerta__contenido">
+                <p class="alerta__parrafo">
+                  Este es el contenido de una alerta informativa que proporciona
+                  información relevante a la persona usuaria sobre el estado del
+                  sistema
+                </p>
+              </div>
+            </div>
+          </article>
+          <article class="alerta advertencia">
+            <div class="alerta__info">
+              <div class="alerta__encabezado">
+                <div class="alerta__info--titulo">
+                  <svg
+                    id="information"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      id="Trazado_28876"
+                      data-name="Trazado 28876"
+                      d="M15.75,20V14h-3v1.5h1.5V20H12v1.5h6V20Z"
+                      transform="translate(-3 -3.5)"
+                      fill="#2A6F4D"
+                    />
+                    <path
+                      id="Trazado_28877"
+                      data-name="Trazado 28877"
+                      d="M15.625,8A1.125,1.125,0,1,0,16.75,9.125,1.125,1.125,0,0,0,15.625,8Z"
+                      transform="translate(-3.625 -2)"
+                      fill="#2A6F4D"
+                    />
+                    <path
+                      id="Trazado_28878"
+                      data-name="Trazado 28878"
+                      d="M12.5,23A10.5,10.5,0,1,1,23,12.5,10.5,10.5,0,0,1,12.5,23Zm0-19.5a9,9,0,1,0,9,9A9,9,0,0,0,12.5,3.5Z"
+                      transform="translate(-0.5 -0.5)"
+                      fill="#2A6F4D"
+                    />
+                    <rect
+                      id="_Transparent_Rectangle_"
+                      data-name="&lt;Transparent Rectangle&gt;"
+                      width="24"
+                      height="24"
+                      fill="none"
+                    />
+                  </svg>
+
+                  <!-- <img
+                    src="/img/alerta_icono.svg"
+                    alt=""
+                    class="alerta__icono"
+                  /> -->
+
+                  <p class="alerta__titulo">Alerta informativa</p>
+                </div>
+                <img
+                  src="/img/cerrar_icono.svg"
+                  alt=""
+                  class="alerta__cerrar--icono"
+                />
+              </div>
+              <div class="alerta__contenido">
+                <p class="alerta__parrafo">
+                  Este es el contenido de una alerta informativa que proporciona
+                  información relevante a la persona usuaria sobre el estado del
+                  sistema
+                </p>
+              </div>
+            </div>
+          </article>
+          <article class="alerta error">
+            <div class="alerta__info">
+              <div class="alerta__encabezado">
+                <div class="alerta__info--titulo">
+                  <img
+                    src="/img/alerta_icono.svg"
+                    alt=""
+                    class="alerta__icono"
+                  />
+                  <p class="alerta__titulo">Alerta informativa</p>
+                </div>
+
+                <img
+                  src="/img/cerrar_icono.svg"
+                  alt=""
+                  class="alerta__cerrar--icono"
+                />
+              </div>
+              <div class="alerta__contenido">
+                <p class="alerta__parrafo">
+                  Este es el contenido de una alerta informativa que proporciona
+                  información relevante a la persona usuaria sobre el estado del
+                  sistema
+                </p>
+              </div>
+            </div>
+          </article>
+        </div>
+      </section>
+    </div>
+  </div>
 </template>
