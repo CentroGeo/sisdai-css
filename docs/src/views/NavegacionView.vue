9b90a34a7c3ddb5bba1e142e0f5e468348135fdc
ciretorres <erictorres.velasco@gmail.com>
2024-01-31 13:27:15 -0600

    subiendo documentación del menú de accesibilidad

--- docs/src/views/NavegacionView.vue
+++ docs/src/views/NavegacionView.vue
@@ -94,6 +94,7 @@
         : "Activar vista movil forzada en menús"
     }}
   </button>
+
   <h3>Gobierno</h3>
   <nav
     class="navegacion navegacion-gobmx"
@@ -219,8 +220,8 @@
       </div>
     </div>
   </nav>
-  <h2>Colapsable</h2>
 
+  <h2>Colapsable</h2>
   <p>
     Los estilos de estos elementos colapsables como elementos independientes aún
     no está definido dentro del sistema de diseño. Consiste en un elemento
@@ -228,7 +229,6 @@
     .activo, entonces muestra una lista de enlaces.
   </p>
   <p>En el siguiente ejemplo, la clase .activo se agrega al hacer click</p>
-
   <div class="contenedor-colapsable" :class="{ activo: colapsable_activo1 }">
     <button
       :aria-expanded="colapsable_activo1"
@@ -354,14 +354,13 @@
       </div>
     </div>
   </menu>
-  <h2>Índice de contenido</h2>
 
+  <h2>Índice de contenido</h2>
   <p>
     El la clase .indice-de-contenido da a su contenido el estilo que se usa para
     el panel lateral derecho que te permite navegar entre distinas secciones del
     documento sin cambiar de vista
   </p>
-
   <div class="indice-de-contenido">
     <p class="titulo-indice">{ { titulo } }</p>
     <nav>
@@ -373,7 +372,73 @@
       </div>
     </nav>
   </div>
-  <p></p>
+  
+  <h3>Menú Accesibilidad</h3>
+  <p>
+    La clase .contenedor-menu-accesibilidad se compone de estilos
+    para darle forma al menú con un botón flotante que abre y cierra
+    la navegación. Un título, seguido de un conjunto de opciones con
+    checkbox que se comunican para avisar cuando alguna funcionalidad
+    está prendida o apagada.
+  </p>
+  <div
+    class="contenedor-menu-accesibilidad"
+    :class="{ abierto: menuAccesibilidadEstaAbierto }"
+    style="position: relative;"
+  >
+    <button
+      class="icono-boton-accesibilidad"
+      :aria-expanded="menuAccesibilidadEstaAbierto ? 'true' : 'false'"
+      @click="alternarAbiertoCerrado"
+      style="padding: 16px 24px;"
+    >
+      <span
+        class="icono-accesibilidad icono-5"
+        aria-hidden="true"
+      />
+      <span class="a11y-solo-lectura">
+        abrir y cerrar menú de accesibilidad
+      </span>
+    </button>
+
+    <menu class="menu-accesibilidad" style="margin: 16px 48px;">
+      <p class="titulo">Herramientas de accesibilidad</p>
+      <hr />
+
+      <div
+        class="controlador-vis m-y-1"
+        v-for="(opcion, idx) in opciones"
+        :key="`opcion-accesibilidad-${idx}`"
+      >
+        <input
+          :id="`${opcion.claseCss}_menu_accesibilidad`"
+          type="checkbox"
+          :value="opcion.claseCss"
+          v-model="clasesSelecciondas"
+          :tabindex="menuAccesibilidadEstaAbierto ? undefined : -1"
+        />
+        <label :for="`${opcion.claseCss}_menu_accesibilidad`">
+          <span
+            class="figura-variable icono-4"
+            :class="opcion.icono"
+            aria-hidden="true"
+          ></span>
+          <span class="nombre-variable">
+            <b> {{ opcion.titulo }} </b>
+          </span>
+        </label>
+      </div>
+      <button
+        class="hipervinculo"
+        :tabindex="menuAccesibilidadEstaAbierto ? undefined : -1"
+        @click="restablecer"
+        :disabled="!clasesSelecciondas.length"
+      >
+        <b>Restablecer</b>
+      </button>
+    </menu>
+  </div>
+
 </template>
 
 <script setup>
@@ -385,4 +450,42 @@ const colapsable_activo2= ref(false);
 const colapsable_activo3 = ref(false);
 
 const navegacion_movil_forzada = ref(false);
+
+const menuAccesibilidadEstaAbierto = ref(true)
+const clasesSelecciondas = ref([])
+
+function alternarAbiertoCerrado() {
+  menuAccesibilidadEstaAbierto.value = !menuAccesibilidadEstaAbierto.value
+}
+
+function restablecer() {
+  clasesSelecciondas.value = []
+}
+
+const opciones = ref([
+  {
+    accion: 'alternarTipografiaAccesible',
+    claseCss: 'a11y-tipografia',
+    icono: 'icono-cambio-tipografia',
+    titulo: 'Cambio de fuente',
+  },
+  {
+    accion: 'alternarEnlacesSubrayados',
+    claseCss: 'a11y-hipervinculos',
+    icono: 'icono-enlace-subrayado',
+    titulo: 'Enlaces subrayados',
+  },
+  {
+    accion: 'alternarVistaSimplificada',
+    claseCss: 'a11y-simplificada',
+    icono: 'icono-vista-simplificada',
+    titulo: 'Mostrar solo texto',
+  },
+  {
+    accion: 'alternarVistaOscura',
+    claseCss: 'a11y-oscura',
+    icono: 'icono-contraste',
+    titulo: 'Vista Oscura',
+  },
+])
 </script>
