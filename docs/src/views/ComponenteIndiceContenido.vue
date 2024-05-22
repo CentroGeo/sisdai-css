b9c8eb5e18f23359b51d292d41a84255810f9aee
flkt <flkt.crnpio@gmail.com>
2024-05-22 13:53:59 -0600

    agregando estilos del componente indice de contenido

--- docs/src/views/ComponenteIndiceContenido.vue
+++ docs/src/views/ComponenteIndiceContenido.vue
@@ -1,24 +1,65 @@
+<script setup>
+import EjemploCodigoVertical from '../componetes/EjemploCodigoVertical.vue'
+</script>
 <template>
-  <div>
 
-    <h1>Índice de contenido <span class="etiqueta">preliminar</span></h1>
-    <div class="contenedor-lectura ">
-      <p>
-        El la clase .indice-de-contenido da a su contenido el estilo que se usa para
-        el panel lateral derecho que te permite navegar entre distinas secciones del
-        documento sin cambiar de vista
-      </p>
+  <div class="flex">
+    <div class="columna-4 menu-lateral-fondo oculto-mov">
     </div>
-    <div class="indice-de-contenido">
-      <p class="titulo-indice" id="indice">Título índice</p>
-      <nav aria-labelledby="indice">
-        <div class="contenedor-indice-de-contenido">
-          <a href="#"> Elemento del índice </a>
-          <a href="#"> Elemento del índice </a>
-          <a href="#"> Elemento del índice </a>
+    <div class="columna-4 columna-orden-3-esc">
+      <div class="indice-contenido">
+        <p 
+          class="indice-contenido-titulo" 
+          id="indicecontenido"
+        >
+          Título índice
+        </p>
+        <nav 
+          aria-labelledby="indicecontenido"
+          class="indice-contenido-contenedor"
+        >
+          <a href="#seccion1"> Sección Uno </a>
+          <a href="#seccion2"> Sección Dos </a>
+          <a href="#seccion3"> Sección Tres </a>
+        </nav>
       </div>
-    </nav>
+    </div>
+    <div class="columna-8">
+      <div class="m-y-maximo contenedor">
+        <div class="ancho-lectura">
+          <h1>Índice de contenido <span class="etiqueta">preliminar</span></h1>
+          <p>
+            Estilos para el componente <code>SisdaiIndiceDeContenido</code> declarado en la biblioteca 
+            <a href="https://codigo.conahcyt.mx/sisdai/sisdai-componentes" target="_blank" rel="noopener noreferrer">sisdai-componentes</a>.
+            Este componente se utiliza como un menú de navegación entre distinas secciones de una vista.
+          </p>
+          <p>
+            El <code>indice-contenido</code> se compone de un título <code>indice-contenido-titulo</code>
+            y un contenedor <code>indice-contenido-contenedor</code> en el cual se espera que contenga hipervínculos locales.
+          </p>
+          <EjemploCodigoVertical 
+            html='
+            <div class="indice-contenido">
+              <p 
+                class="indice-contenido-titulo" 
+                id="indicecontenido"
+              >
+                Título índice
+              </p>
+              <nav 
+                aria-labelledby="indicecontenido"
+                class="indice-contenido-contenedor"
+              >
+                <a href="#seccion1"> Sección Uno </a>
+                <a href="#seccion2"> Sección Dos </a>
+                <a href="#seccion3"> Sección Tres </a>
+              </nav>
+            </div>
+          ' />
+        </div>
+      </div>
+    </div>
+    
   </div>
 
-</div>
 </template>
--- docs/src/views/ComponenteMenuLateral.vue
+++ docs/src/views/ComponenteMenuLateral.vue
@@ -114,127 +114,128 @@ import EjemploCodigoVertical from '../componetes/EjemploCodigoVertical.vue'
         </nav>
 
       </div>
-      <div class="columna-12 columna-8-mov">
-        <div class="contenedor m-y-maximo alineacion-izquierda ancho-lectura">
-          <h1>Menú lateral <span class="etiqueta">preliminar</span></h1>
-          <p>
-            Estilos para el componente <code>SisdaiMenuLateral</code> declarado en la biblioteca 
-            <a href="https://codigo.conahcyt.mx/sisdai/sisdai-componentes" target="_blank" rel="noopener noreferrer">sisdai-componentes</a>.
-          </p>
+      <div class="columna-12">
+        <div class="contenedor m-y-maximo">
+          <div class="contenedor alineacion-izquierda ancho-lectura">
 
-          <p>
-            Este componente <code>menu-lateral</code> tiene la logica de los colapsables en versión móvil, 
-            es decir que en celulares se muestra el botón <code>menu-lateral-boton</code> a través del cual 
-            se agrega o quita la clase <code>abierto</code> directamente sobre el <code>menu-lateral</code> 
-            para mostrar u ocultar su contenido.
-          </p>
-          <EjemploCodigoVertical
-            :tiene_ejemplo="false"
-            html='
-            <nav class="menu-lateral"> <- aqui se agrega .abierto (notas estado abierto)
-
-              <button 
-                class="menu-lateral-boton" 
-                aria-controls="ID-UNICO" 
-                aria-expanded="false" <- solo en móvil <- aqui se cambia a true (notas estado abierto)
-                aria-label="Abrir/Cerrar navegación"
-              >
-                <span aria-hidden="true" class="pictograma-angulo-derecho"></span>
-              </button>
-
-              <div 
-                class="menu-lateral-contenedor"
-                id="ID-UNICO"
-                aria-hidden="true" <- solo en móvil <- se elimina este atributo (notas estado abierto)
-              >
-
-                <!-- contenido que se muestra u oculta en móvil -->
-
-              </div>
-            </nav>
-          '/>
-          <p>
-            Dentro del componente, se encuentra el contenedor <code>menu-lateral-contenedor</code> que se espera
-            contenga listas anidadas de hipervínculos y componentes de contenido colapsable <code>SisdaiColapsableNavegacion</code>
-          </p>
-
-          <EjemploCodigoVertical
-            :tiene_ejemplo="false"
-            html='
-            <nav class="menu-lateral abierto">
-
-              <button 
-                class="menu-lateral-boton" 
-                aria-controls="ID-UNICO-DOS" 
-                aria-expanded="true" 
-                aria-label="Abrir/Cerrar navegación"
-              >
-                <span aria-hidden="true" class="pictograma-angulo-derecho"></span>
-              </button>
-
-              <div 
-                class="menu-lateral-contenedor"
-                id="ID-UNICO-DOS"
-              >
-                <ul>
-                  <li>
-                    <router-link to="/">
-                      router link inicio prueba
-                    </router-link>
-                  </li>
-                  <li>
-                    <a href="#">
-                      hipervinculo
-                    </a>
-                  </li>
-                  <li class="colapsable">
-                    <button 
-                      class="colapsable-boton" 
-                      aria-controls="ID-UNICO-TRES" 
-                      aria-expanded="false" 
-                      aria-label="Abrir/Cerrar colapsable"
-                    >
-                      Colapsable
-                      <span aria-hidden="true" class="pictograma-angulo-derecho"></span>
-                    </button>
-                    <div 
-                      class="colapsable-contenedor" 
-                      id="ID-UNICO-TRES" 
-                      aria-hidden="true"
-                    >
-                      <ul>
-                        <li>
-                          <a href="#">Enlace colapsado</a>
-                        </li>
-                      </ul>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          '/>
-          
-          <p>
-            La clase <code>menu-lateral-fondo</code>, no forma parte del <code>menu-lateral</code>, pero ayuda a poner el mismo
-            color de fondo en la columna en donde se encuentre el menu para que en la vista de escritorio se complete visualmente
-            la sección aunque el menu tenga poca altura. 
-          </p>
-          <EjemploCodigoVertical
-            :tiene_ejemplo="false"
-            html='
-            <div class="columna-4 menu-lateral-fondo">
-
-              <nav class="menu-lateral">
-
-                <!-- contenido del menu -->
-
+            <h1>Menú lateral <span class="etiqueta">preliminar</span></h1>
+            <p>
+              Estilos para el componente <code>SisdaiMenuLateral</code> declarado en la biblioteca 
+              <a href="https://codigo.conahcyt.mx/sisdai/sisdai-componentes" target="_blank" rel="noopener noreferrer">sisdai-componentes</a>.
+            </p>
+  
+            <p>
+              Este componente <code>menu-lateral</code> tiene la logica de los colapsables en versión móvil, 
+              es decir que en celulares se muestra el botón <code>menu-lateral-boton</code> a través del cual 
+              se agrega o quita la clase <code>abierto</code> directamente sobre el <code>menu-lateral</code> 
+              para mostrar u ocultar su contenido.
+            </p>
+            <EjemploCodigoVertical
+              :tiene_ejemplo="false"
+              html='
+              <nav class="menu-lateral"> <- aqui se agrega .abierto (notas estado abierto)
+  
+                <button 
+                  class="menu-lateral-boton" 
+                  aria-controls="ID-UNICO" 
+                  aria-expanded="false" <- solo en móvil <- aqui se cambia a true (notas estado abierto)
+                  aria-label="Abrir/Cerrar navegación"
+                >
+                  <span aria-hidden="true" class="pictograma-angulo-derecho"></span>
+                </button>
+  
+                <div 
+                  class="menu-lateral-contenedor"
+                  id="ID-UNICO"
+                  aria-hidden="true" <- solo en móvil <- se elimina este atributo (notas estado abierto)
+                >
+  
+                  <!-- contenido que se muestra u oculta en móvil -->
+  
+                </div>
               </nav>
-            </div>
-          '/>
-          
+            '/>
+            <p>
+              Dentro del componente, se encuentra el contenedor <code>menu-lateral-contenedor</code> que se espera
+              contenga listas anidadas de hipervínculos y componentes de contenido colapsable <code>SisdaiColapsableNavegacion</code>
+            </p>
+  
+            <EjemploCodigoVertical
+              :tiene_ejemplo="false"
+              html='
+              <nav class="menu-lateral abierto">
+  
+                <button 
+                  class="menu-lateral-boton" 
+                  aria-controls="ID-UNICO-DOS" 
+                  aria-expanded="true" 
+                  aria-label="Abrir/Cerrar navegación"
+                >
+                  <span aria-hidden="true" class="pictograma-angulo-derecho"></span>
+                </button>
+  
+                <div 
+                  class="menu-lateral-contenedor"
+                  id="ID-UNICO-DOS"
+                >
+                  <ul>
+                    <li>
+                      <router-link to="/">
+                        router link inicio prueba
+                      </router-link>
+                    </li>
+                    <li>
+                      <a href="#">
+                        hipervinculo
+                      </a>
+                    </li>
+                    <li class="colapsable">
+                      <button 
+                        class="colapsable-boton" 
+                        aria-controls="ID-UNICO-TRES" 
+                        aria-expanded="false" 
+                        aria-label="Abrir/Cerrar colapsable"
+                      >
+                        Colapsable
+                        <span aria-hidden="true" class="pictograma-angulo-derecho"></span>
+                      </button>
+                      <div 
+                        class="colapsable-contenedor" 
+                        id="ID-UNICO-TRES" 
+                        aria-hidden="true"
+                      >
+                        <ul>
+                          <li>
+                            <a href="#">Enlace colapsado</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            '/>
+            
+            <p>
+              La clase <code>menu-lateral-fondo</code>, no forma parte del <code>menu-lateral</code>, pero ayuda a poner el mismo
+              color de fondo en la columna en donde se encuentre el menu para que en la vista de escritorio se complete visualmente
+              la sección aunque el menu tenga poca altura. 
+            </p>
+            <EjemploCodigoVertical
+              :tiene_ejemplo="false"
+              html='
+              <div class="columna-4 menu-lateral-fondo">
+  
+                <nav class="menu-lateral">
+  
+                  <!-- contenido del menu -->
+  
+                </nav>
+              </div>
+            '/>
+          </div>
         </div>
       </div>
     </div>
-
   </div>
 </template>
