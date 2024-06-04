2e9e9efd2db77f790d2d1997f9710463e21a93bf
flkt <flkt.crnpio@gmail.com>
2024-06-04 17:59:34 -0600

    eliminando nota de uso de componente deslizante

--- docs/src/views/ComponenteAudio.vue
+++ docs/src/views/ComponenteAudio.vue
@@ -11,12 +11,6 @@ import EjemploCodigoVertical from '../componetes/EjemploCodigoVertical.vue'
         <a href="https://codigo.conahcyt.mx/sisdai/sisdai-componentes" target="_blank" rel="noopener noreferrer">sisdai-componentes</a>.
         Este componente permite reproducir audios en el navegador.
       </p>
-      <p>
-        <b>Nota:</b> para mostrar el ejemplo se utiliza un campo <code lang="en">input type="range"</code>
-        que debe ser reemplazado por el componente <code>SisdaiControlDeslizante</code>,
-        al que se le debe agregar el nombre de la clase y el atributo <code lang="en">aria-label</code> 
-        del campo, como se muestra a continuación.
-      </p>
       
       <EjemploCodigoVertical
         html='
