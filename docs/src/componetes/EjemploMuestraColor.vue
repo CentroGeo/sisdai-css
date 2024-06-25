9db46f7b340bcb455859ba0390bac2a18611de0c
flkt <flkt.crnpio@gmail.com>
2024-06-25 13:24:04 -0600

    eliminando hexadecimal en muestra de color

--- docs/src/componetes/EjemploMuestraColor.vue
+++ docs/src/componetes/EjemploMuestraColor.vue
@@ -8,17 +8,10 @@
       type: String,
       required: true
     },
-    hexadecimal: {
-      type: String,
-      required: true
-    }
   })
-
-
 </script>
 <template>
   <div class="p-3 borde borde-color-secundario texto-tamanio-1" :style="{'background-color': variable}">
     <code :style="{'color': texto}">{{ variable }}</code>
-    <!-- <code :style="{'color': texto}">{{ hexadecimal }}</code> -->
   </div>
 </template>
--- docs/src/views/FundamentosColor.vue
+++ docs/src/views/FundamentosColor.vue
@@ -35,52 +35,52 @@ import EjemploMuestraColor from '../componetes/EjemploMuestraColor.vue'
     </div>
     <div class="flex m-y-5">
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-primario-4)" hexadecimal="#0058cc" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-primario-4)" texto="#fff"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-primario-3)" hexadecimal="#0068FF" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-primario-3)" texto="#fff"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-primario-2)" hexadecimal="#6692FF" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-primario-2)" texto="#000"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-primario-1)" hexadecimal="#80AAFF" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-primario-1)" texto="#000"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-secundario-12)" hexadecimal="#102543" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-secundario-12)" texto="#fff"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-secundario-11)" hexadecimal="#142a48" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-secundario-11)" texto="#fff"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-secundario-10)" hexadecimal="#1B304E" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-secundario-10)" texto="#fff"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-secundario-9)" hexadecimal="#2A3F5D" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-secundario-9)" texto="#fff"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-secundario-8)" hexadecimal="#364A69" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-secundario-8)" texto="#fff"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-secundario-7)" hexadecimal="#5E7392" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-secundario-7)" texto="#fff"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-secundario-6)" hexadecimal="#889DBB" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-secundario-6)" texto="#000"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-secundario-5)" hexadecimal="#B1C6E4" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-secundario-5)" texto="#000"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-secundario-4)" hexadecimal="#CCE1FF" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-secundario-4)" texto="#000"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-secundario-3)" hexadecimal="#DBEAFF" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-secundario-3)" texto="#000"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-secundario-2)" hexadecimal="#F0F6FF" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-secundario-2)" texto="#000"/>
       </div>
       <div class="columna-4">
-        <EjemploMuestraColor variable="var(--color-secundario-1)" hexadecimal="#F8FBFF" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-secundario-1)" texto="#000"/>
       </div>
     </div>
 
@@ -99,58 +99,58 @@ import EjemploMuestraColor from '../componetes/EjemploMuestraColor.vue'
     <div class="flex flex-contenido-centrado">
       <div class="columna-4">
         <p>Neutro</p>
-        <EjemploMuestraColor variable="var(--color-neutro-7)" hexadecimal="#000000" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-neutro-6)" hexadecimal="#141414" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-neutro-5)" hexadecimal="#434344" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-neutro-4)" hexadecimal="#767677" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-neutro-3)" hexadecimal="#AAAAAA" texto="#000"/>
-        <EjemploMuestraColor variable="var(--color-neutro-2)" hexadecimal="#DDDDDD" texto="#000"/>
-        <EjemploMuestraColor variable="var(--color-neutro-1)" hexadecimal="#F5F5F5" texto="#000"/>
-        <EjemploMuestraColor variable="var(--color-neutro-0)" hexadecimal="#FFFFFF" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-neutro-7)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-neutro-6)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-neutro-5)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-neutro-4)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-neutro-3)" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-neutro-2)" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-neutro-1)" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-neutro-0)" texto="#000"/>
       </div>
 
       <div class="columna-4">
         <p>Interactivo</p>
-        <EjemploMuestraColor variable="var(--color-interactivo-6)" hexadecimal="#9233FF" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-interactivo-5)" hexadecimal="#CF99FF" texto="#000"/>
-        <EjemploMuestraColor variable="var(--color-interactivo-4)" hexadecimal="#8999CC" texto="#000"/>
-        <EjemploMuestraColor variable="var(--color-interactivo-3)" hexadecimal="#668BFF" texto="#000"/>
-        <EjemploMuestraColor variable="var(--color-interactivo-2)" hexadecimal="#356CCC" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-interactivo-1)" hexadecimal="#005DFF" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-interactivo-6)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-interactivo-5)" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-interactivo-4)" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-interactivo-3)" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-interactivo-2)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-interactivo-1)" texto="#fff"/>
       </div>
     </div>
 
     <div class="flex">
       <div class="columna-4">
         <p>Informativo</p>
-        <EjemploMuestraColor variable="var(--color-informativo-4)" hexadecimal="#172136" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-informativo-3)" hexadecimal="#19439C" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-informativo-2)" hexadecimal="#66A5FE" texto="#000"/>
-        <EjemploMuestraColor variable="var(--color-informativo-1)" hexadecimal="#ECF2FF" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-informativo-4)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-informativo-3)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-informativo-2)" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-informativo-1)" texto="#000"/>
       </div>
 
       <div class="columna-4">
         <p>Confirmación</p>
-        <EjemploMuestraColor variable="var(--color-confirmacion-4)" hexadecimal="#0D2619" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-confirmacion-3)" hexadecimal="#2A6F4D" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-confirmacion-2)" hexadecimal="#1BD97C" texto="#000"/>
-        <EjemploMuestraColor variable="var(--color-confirmacion-1)" hexadecimal="#E7FBF1" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-confirmacion-4)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-confirmacion-3)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-confirmacion-2)" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-confirmacion-1)" texto="#000"/>
       </div>
 
       <div class="columna-4">
         <p>Alerta</p>
-        <EjemploMuestraColor variable="var(--color-alerta-4)" hexadecimal="#201C11" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-alerta-3)" hexadecimal="#916802" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-alerta-2)" hexadecimal="#FFDC69" texto="#000"/>
-        <EjemploMuestraColor variable="var(--color-alerta-1)" hexadecimal="#FFF7E3" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-alerta-4)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-alerta-3)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-alerta-2)" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-alerta-1)" texto="#000"/>
       </div>
     
       <div class="columna-4">
         <p>Error</p>
-        <EjemploMuestraColor variable="var(--color-error-4)" hexadecimal="#341116" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-error-3)" hexadecimal="#940B1C" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-error-2)" hexadecimal="#FF6969" texto="#000"/>
-        <EjemploMuestraColor variable="var(--color-error-1)" hexadecimal="#FCDADE" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-error-4)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-error-3)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-error-2)" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-error-1)" texto="#000"/>
       </div>
     </div>
 
@@ -166,22 +166,22 @@ import EjemploMuestraColor from '../componetes/EjemploMuestraColor.vue'
     <div class="flex flex-contenido-centrado">
       <div class="columna-4">
         <p>Gobierno de México</p>
-        <EjemploMuestraColor variable="var(--color-mx-5)" hexadecimal="#434344" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-mx-4)" hexadecimal="#767677" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-mx-3)" hexadecimal="#AAAAAA" texto="#000"/>
-        <EjemploMuestraColor variable="var(--color-mx-2)" hexadecimal="#DDDDDD" texto="#000"/>
-        <EjemploMuestraColor variable="var(--color-mx-1)" hexadecimal="#F5F5F5" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-mx-0)" hexadecimal="#FFFFFF" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-mx-5)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-mx-4)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-mx-3)" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-mx-2)" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-mx-1)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-mx-0)" texto="#000"/>
       </div>
 
       <div class="columna-4">
         <p>Conahcyt</p>
-        <EjemploMuestraColor variable="var(--color-conahcyt-5)" hexadecimal="#CF99FF" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-conahcyt-4)" hexadecimal="#8999CC" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-conahcyt-3)" hexadecimal="#668BFF" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-conahcyt-2)" hexadecimal="#356CCC" texto="#fff"/>
-        <EjemploMuestraColor variable="var(--color-conahcyt-1)" hexadecimal="#005DFF" texto="#000"/>
-        <EjemploMuestraColor variable="var(--color-conahcyt-0)" hexadecimal="#005DFF" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-conahcyt-5)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-conahcyt-4)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-conahcyt-3)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-conahcyt-2)" texto="#fff"/>
+        <EjemploMuestraColor variable="var(--color-conahcyt-1)" texto="#000"/>
+        <EjemploMuestraColor variable="var(--color-conahcyt-0)" texto="#000"/>
       </div>
     </div>
 
