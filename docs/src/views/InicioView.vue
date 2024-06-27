ee7e5b74b9078ac20ed7a3df0574d02b0d647c59
flkt <flkt.crnpio@gmail.com>
2024-06-27 14:29:49 -0600

    agregando link de version de la biblioteca

--- docs/src/views/InicioView.vue
+++ docs/src/views/InicioView.vue
@@ -8,5 +8,15 @@ import pkg from '../../../package.json'
       Esta es la documentación de la biblioteca de estilos del Sistema de Diseño y Accesibilidad para la
       Investigación (<a href="https://sisdai.conacyt.mx/" target="_blank">Sisdai</a>).
     </p>
+    <p class="parrafo-texto-alto">
+      <a 
+        :href="`https://codigo.conahcyt.mx/sisdai/sisdai-css/-/tree/v${pkg.version}`"
+        target="_blank" 
+        rel="noref noopener"
+        >
+        <span class="pictograma-social-github"></span>
+        sisdai-css#{{ pkg.version }}
+      </a>
+    </p>
   </div>
 </template>
