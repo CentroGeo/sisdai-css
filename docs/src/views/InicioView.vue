d928a1c4337c7421af297d865207068bdb391b25
Paulina Pradel <crip.dadsig019@conahcyt.mx>
2024-07-09 13:42:12 -0600

    se cambian imágenes de inicio de biblioteca

--- docs/src/views/InicioView.vue
+++ docs/src/views/InicioView.vue
@@ -9,7 +9,7 @@
     <div class="flex flex-contenido-centrado m-y-5">
       <div class="columna-16">
         <a class="tarjeta tarjeta-hipervinculo-externo" href="https://codigo.conahcyt.mx/sisdai/sisdai-css" target="_blank" rel="noopener noreferrer">
-          <img class="tarjeta-imagen" src="https://cdn.conacyt.mx/sisdai/recursos/imagenes/documentacion/perro-3.jpg" alt="" />
+          <img class="tarjeta-imagen" src="https://cdn.conacyt.mx/sisdai/recursos/imagenes/documentacion/gitlab.png" alt="" />
           <div class="tarjeta-degradado">
             <div class="tarjeta-cuerpo">
               <p class="h4">Repositorio sisdai-css <span class="pictograma-flecha-arriba-derecha"></span></p>
@@ -41,7 +41,7 @@
 
       <div class="columna-16">
         <a class="tarjeta tarjeta-hipervinculo-externo" href="https://sisdai.conahcyt.mx/" target="_blank" rel="noopener noreferrer">
-          <img class="tarjeta-imagen" src="https://cdn.conacyt.mx/sisdai/recursos/imagenes/documentacion/perro-3.jpg" alt="" />
+          <img class="tarjeta-imagen" src="https://cdn.conacyt.mx/sisdai/recursos/imagenes/documentacion/sisdai.png" alt="" />
           <div class="tarjeta-degradado">
             <div class="tarjeta-cuerpo">
               <p class="h4">Ir a Sisdai <span class="pictograma-flecha-arriba-derecha"></span></p>
