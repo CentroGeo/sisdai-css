db88e5bdfa6942135555ae0b073fb070f9a9e65c
flkt <flkt.crnpio@gmail.com>
2024-07-25 16:18:25 -0600

    restaurando estilos naturales de tablas. creando estilos de listas. creando clase lista secuenciada. actualizando documentacion

--- docs/src/views/ElementosListas.vue
+++ docs/src/views/ElementosListas.vue
@@ -12,18 +12,15 @@ import EjemploCodigo from '../componetes/EjemploCodigo.vue'
         los requerimientos del HTML semántico. Esto asegura una integración 
         coherente y accesible dentro de la estructura web. 
       </p>
-      <ul>
-        <li>
-          Listas ordenadas <code>&lt;ol/></code>: se pueden anidar, incrementando 
-          la numeración de los subelementos en cada nivel. Esto ayuda a mantener una 
-          jerarquía clara y entender la estructura de los elementos listados.
-        </li>
-        <li>
-          Listas desordenadas <code>&lt;ul/></code>: también admiten anidación con un 
-          espacio de sangría que se incrementa con cada nivel, facilitando la distinción 
-          visual entre diferentes niveles y subniveles. 
-        </li>
-      </ul>
+      <p>
+        Listas ordenadas y desordenadas <code>&lt;ul/></code> se pueden anidar con un 
+        espacio de sangría que se incrementa con cada nivel, facilitando la distinción 
+        visual entre diferentes niveles y subniveles.
+      </p>
+      <p>
+        Listas descriptivas <code>&lt;dl/></code> requieren a cada término <code>dt</code>
+        agregarle su correspondiente descripción <code>dd</code>. Estas listas no se anidan.
+      </p>
     </div>
     <div class="flex flex-contenido-centrado">
       <div class="columna-4">
@@ -79,10 +76,8 @@ import EjemploCodigo from '../componetes/EjemploCodigo.vue'
         <dl>
           <dt>Término</dt>
           <dd>Descripción del término</dd>
-
           <dt>Término</dt>
           <dd>Descripción del término</dd>
-
           <dt>Término</dt>
           <dd>Descripción del término</dd>
         </dl>
@@ -90,6 +85,198 @@ import EjemploCodigo from '../componetes/EjemploCodigo.vue'
       />
       </div>
     </div>
+
+    <div class="flex flex-contenido-centrado">
+      <div class="columna-4">
+        <EjemploCodigo
+          tipo='HTML'
+          codigo='
+          <ol>
+            <li>Lista Nivel 1</li>
+            <li>
+              Lista Nivel 1
+              <ul>
+                <li>
+                  Lista Nivel 2
+                  <ul>
+                    <li>ListaNivel 3</li>
+                  </ul>
+                </li>
+                <li>Lista Nivel 2</li>
+              </ul>
+            </li>
+            <li>Lista Nivel 1</li>
+          </ol>
+          '
+        />
+      </div>
+      <div class="columna-4">
+        <EjemploCodigo
+          tipo='HTML'
+          codigo='
+          <ul>
+            <li>Lista Nivel 1</li>
+            <li>
+              Lista Nivel 1
+              <ol>
+                <li>
+                  Lista Nivel 2
+                  <ol>
+                    <li>Lista Nivel 3</li>
+                  </ol>
+                </li>
+                <li>Lista Nivel 2</li>
+              </ol>
+            </li>
+            <li>Lista Nivel 1</li>
+          </ul>
+          '
+        />
+      </div>
+    </div>
+
+    <div class="ancho-lectura">
+      <h2 class="m-t-10">Tipos de listas</h2>
+      <p>
+        Existen clases que se puede agregar a listas ordenadas y desordenadas para cambiar el tipo de estilo de la lista.
+        Estas clases solo afectan a la lista en la que se agregue, en caso de tener listas anidadas se puede agregar cualquier otro tipo
+        que sea requerido.
+      </p>
+    </div>
+
+    <div class="flex flex-contenido-centrado">
+      <div class="columna-4">
+        <EjemploCodigo
+          tipo='HTML'
+          codigo='
+          <ol class="lista-latina">
+            <li>.lista-latina</li>
+            <li>
+              Lista Nivel 1
+              <ul>
+                <li>
+                  Lista Nivel 2
+                </li>
+                <li>Lista Nivel 2</li>
+              </ul>
+            </li>
+            <li>Lista Nivel 1</li>
+          </ol>
+          '
+        />
+      </div>
+      <div class="columna-4">
+        <EjemploCodigo
+          tipo='HTML'
+          codigo='
+          <ol class="lista-romana">
+            <li>.lista-romana</li>
+            <li>
+              Lista Nivel 1
+              <ol class="lista-decimal">
+                <li>
+                  Lista Nivel 2
+                </li>
+                <li>Lista Nivel 2</li>
+              </ol>
+            </li>
+            <li>Lista Nivel 1</li>
+          </ol>
+          '
+        />
+      </div>
+      <div class="columna-4">
+        <EjemploCodigo
+          tipo='HTML'
+          codigo='
+          <ol class="lista-decimal">
+            <li>.lista-decimal</li>
+            <li>
+              Lista Nivel 1
+              <ul>
+                <li>
+                  Lista Nivel 2
+                </li>
+                <li>Lista Nivel 2</li>
+              </ul>
+            </li>
+            <li>Lista Nivel 1</li>
+          </ol>
+          '
+        />
+      </div>
+      <div class="columna-4">
+        <EjemploCodigo
+          tipo='HTML'
+          codigo='
+          <ol class="lista-sin-estilo">
+            <li>.lista-sin-estilo</li>
+            <li>
+              Lista Nivel 1
+              <ul>
+                <li>
+                  Lista Nivel 2
+                </li>
+                <li>Lista Nivel 2</li>
+              </ul>
+            </li>
+            <li>Lista Nivel 1</li>
+          </ol>
+          '
+        />
+      </div>
+    </div>
+
+    <div class="ancho-lectura">
+      <h2 class="m-t-10">Tipo especial de listas ordenadas</h2>
+      <p>
+        La lista <code>.lista-secuenciada</code> se pueden anidar, incrementando 
+        la numeración de los subelementos en cada nivel. Esto ayuda a mantener una 
+        jerarquía clara y entender la estructura de los elementos listados.
+      </p>
+      <p>
+        Esta clase se puede utilizar en listas ordenadas y desordenadas 
+        pero siempre mantiene el estilo que se demuestra en el ejemplo, incluso si se 
+        agregara una clase a alguna lista anidada.
+      </p>
+    </div>
+
+    <div class="flex flex-contenido-centrado">
+      <div class="columna-8">
+        <EjemploCodigo
+          tipo='HTML'
+          codigo='
+          <ul class="lista-secuenciada">
+            <li>lista-secuenciada</li>
+            <li>
+              Lista Nivel 1
+              <ol>
+                <li>
+                  Lista Nivel 2
+                  <ol>
+                    <li>
+                      Lista Nivel 3
+                      <ol>
+                        <li>
+                          Lista Nivel 4
+                        </li>
+                        <li>Lista Nivel 4</li>
+                      </ol>
+                    </li>
+                    <li>Lista Nivel 3</li>
+                  </ol>
+                </li>
+                <li>Lista Nivel 2</li>
+              </ol>
+            </li>
+            <li>Lista Nivel 1</li>
+          </ul>
+          '
+        />
+      </div>
+     
+    </div>
+
 
   </div>
 
