eaeffc5a9a9c924014188f558c36d42f6e256d53
AlonsoCortes <alo.acs@gmail.com>
2024-08-28 14:48:11 -0600

    agregar tarjetas inicio

--- docs/src/views/InicioView.vue
+++ docs/src/views/InicioView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="contenedor ancho-lectura p-b-maximo">
+  <div class="contenedor ancho-lectura p-b-minimo">
     <h1 class="texto-centrado">sisdai-css</h1>
     <p>
       Ésta es la documentación para personas desarrolladoras de la biblioteca 
@@ -12,20 +12,17 @@
       agrega clases para ayudar a la accesibilidad digital.
     </p>
 
-
-
-    <div class="flex flex-contenido-centrado m-y-5">
-      <div class="columna-16">
-        <a class="tarjeta tarjeta-hipervinculo-externo" href="https://codigo.conahcyt.mx/sisdai/sisdai-css" target="_blank" rel="noopener noreferrer">
-          <img class="tarjeta-imagen" src="https://cdn.conacyt.mx/sisdai/recursos/imagenes/documentacion/gitlab.png" alt="" />
-          <div class="tarjeta-degradado">
-            <div class="tarjeta-cuerpo">
-              <p class="h4">Ir al repositorio sisdai-css <span class="pictograma-flecha-arriba-derecha"></span></p>
-            </div>
-          </div>
-        </a>
-      </div>
-
+<!--    <div class="flex flex-contenido-centrado m-y-5">-->
+<!--      <div class="columna-16">-->
+<!--        <a class="tarjeta tarjeta-hipervinculo-externo" href="https://codigo.conahcyt.mx/sisdai/sisdai-css" target="_blank" rel="noopener noreferrer">-->
+<!--          <img class="tarjeta-imagen" src="https://cdn.conacyt.mx/sisdai/recursos/imagenes/documentacion/gitlab.png" alt="" />-->
+<!--          <div class="tarjeta-degradado">-->
+<!--            <div class="tarjeta-cuerpo">-->
+<!--              <p class="h4">Ir al repositorio sisdai-css <span class="pictograma-flecha-arriba-derecha"></span></p>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </a>-->
+<!--      </div>-->
 <!--      <div class="columna-8">-->
 <!--        <a class="tarjeta tarjeta-hipervinculo-externo" href="/como-empezar" target="_self" rel="noopener noreferrer">-->
 <!--          <img class="tarjeta-imagen" src="https://cdn.conahcyt.mx/sisdai/recursos/imagenes/documentacion/asha.jpg" alt="" />-->
@@ -47,6 +44,163 @@
 <!--        </a>-->
 <!--      </div>-->
 
-    </div>
+<!--    </div>-->
+  </div>
+
+  <div class="contenedor ancho-fijo p-b-maximo">
+    <div class="flex">
+        <div class="columna-8">
+          <a 
+          class="tarjeta tarjeta-hipervinculo-interno"
+          href="/accesibilidad/cambio-fuente/"
+          >
+            <img
+              loading="lazy"
+              class="tarjeta-imagen"
+              src="https://cdn.conahcyt.mx/sisdai/recursos/imagenes/inicio/accesibilidad.png"
+              alt=""
+            />
+            <div class="tarjeta-cuerpo">
+              <h4>Accesibilidad</h4>
+              <p>
+                Funciones y características para facilitar la navegación a la 
+                mayor cantidad de personas usuarias contemplando la diversidad 
+                funcional y de edades.
+              </p>
+            </div>
+          </a>
+        </div>
+
+        <div class="columna-8">
+          <a 
+          class="tarjeta tarjeta-hipervinculo-interno"
+          href="/fundamentos/color/"
+          >
+            <img
+              loading="lazy"
+              class="tarjeta-imagen"
+              src="https://cdn.conahcyt.mx/sisdai/recursos/imagenes/inicio/fundamentos.png"
+              alt=""
+            />
+            <div class="tarjeta-cuerpo">
+              <h4>Fundamentos</h4>
+              <p>
+                Las guías básicas de elementos abstractos para iniciar
+                interfaces de usuaria en las que se basa e inspira el Sisdai.
+              </p>
+            </div>
+          </a>
+        </div>
+
+        <div class="columna-8">
+          <a 
+          class="tarjeta tarjeta-hipervinculo-interno"
+          href="/elementos/botones/"
+          >
+            <img
+              loading="lazy"
+              class="tarjeta-imagen"
+              src="https://cdn.conahcyt.mx/sisdai/recursos/imagenes/inicio/elementos.png"
+              alt=""
+            />
+            <div class="tarjeta-cuerpo">
+              <h4>Elementos</h4>
+              <p>
+                Unidades esenciales con estados de interacción que en conjunto 
+                permiten comunicar o relizar algo en especifico dentro de la interfaz.
+              </p>
+            </div>
+          </a>
+        </div>
+
+        <div class="columna-8">
+          <a 
+          class="tarjeta tarjeta-hipervinculo-interno"
+          href="/elementos-compuestos/botones-pictogramas/"
+          >
+            <img
+              loading="lazy"
+              class="tarjeta-imagen"
+              src="https://cdn.conahcyt.mx/sisdai/recursos/imagenes/inicio/compuesto.png"
+              alt=""
+            />
+            <div class="tarjeta-cuerpo">
+              <h4>Compuestos</h4>
+              <p>
+                Elementos agrupados reutilizables que permiten componen la interfaz
+                de usuaria de manera fácil, modular, navegable y accesible.
+              </p>
+            </div>
+          </a>
+        </div>
+
+        <div class="columna-8">
+          <a 
+          class="tarjeta tarjeta-hipervinculo-interno"
+          href="/componentes/audio/"
+          >
+            <img
+              loading="lazy"
+              class="tarjeta-imagen"
+              src="https://cdn.conahcyt.mx/sisdai/recursos/imagenes/inicio/componentes.png"
+              alt=""
+            />
+            <div class="tarjeta-cuerpo">
+              <h4>Componentes</h4>
+              <p>
+                Conjunto de estructuras y estilos de elementos verificados en 
+                términos de accesibilidad y usabilidad para la creación de 
+                componentes modulares. Son utilizados por la biblioteca sisdai-componentes.
+              </p>
+            </div>
+          </a>
+        </div>
+
+        <div class="columna-8">
+          <a 
+          class="tarjeta tarjeta-hipervinculo-interno"
+          href="/visualizaciones/viscontenedor/"
+          >
+            <img
+              loading="lazy"
+              class="tarjeta-imagen"
+              src="https://cdn.conahcyt.mx/sisdai/recursos/imagenes/inicio/visualizaciones.png"
+              alt=""
+            />
+            <div class="tarjeta-cuerpo">
+              <h4>Visualizaciones</h4>
+              <p>
+                Elementos para la creación de componentes creados específicamente
+                para visualizar datos de forma fácil, práctica y accesible. Son
+                utilizados por las bibliotecas sisdai-gráficas y sisdai-mapas.
+              </p>
+            </div>
+          </a>
+        </div>
+
+        <div class="columna-8">
+          <a 
+          class="tarjeta tarjeta-hipervinculo-interno"
+          href="/auxiliares/bordes/"
+          >
+            <img
+              loading="lazy"
+              class="tarjeta-imagen"
+              src="https://cdn.conahcyt.mx/sisdai/recursos/imagenes/inicio/auxiliares.png"
+              alt=""
+            />
+            <div class="tarjeta-cuerpo">
+              <h4>Auxiliares</h4>
+              <p>
+                Clases reutilizables basadas en los fundamentos del Sidai que
+                nos permiten personalizar los estilos de la interfaz de usuaria.
+              </p>
+            </div>
+          </a>
+        </div>
+
+      </div>
+
+
   </div>
 </template>
