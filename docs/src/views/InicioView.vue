f5459a44dfd86470d3e2929d5320cb276102c99c
AlonsoCortes <alo.acs@gmail.com>
2024-08-28 16:52:27 -0600

    correccion div tarjetas

--- docs/src/views/InicioView.vue
+++ docs/src/views/InicioView.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="contenedor ancho-lectura p-b-minimo">
+  <div class="contenedor ancho-fijo p-b-minimo">
+    <div class="contenedor ancho-lectura p-b-minimo">
     <h1 class="texto-centrado">sisdai-css</h1>
     <p>
       Ésta es la documentación para personas desarrolladoras de la biblioteca 
@@ -11,43 +12,9 @@
       navegadores, estandariza estilos desde las etiquetas de HTML y 
       agrega clases para ayudar a la accesibilidad digital.
     </p>
-
-<!--    <div class="flex flex-contenido-centrado m-y-5">-->
-<!--      <div class="columna-16">-->
-<!--        <a class="tarjeta tarjeta-hipervinculo-externo" href="https://codigo.conahcyt.mx/sisdai/sisdai-css" target="_blank" rel="noopener noreferrer">-->
-<!--          <img class="tarjeta-imagen" src="https://cdn.conacyt.mx/sisdai/recursos/imagenes/documentacion/gitlab.png" alt="" />-->
-<!--          <div class="tarjeta-degradado">-->
-<!--            <div class="tarjeta-cuerpo">-->
-<!--              <p class="h4">Ir al repositorio sisdai-css <span class="pictograma-flecha-arriba-derecha"></span></p>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </a>-->
-<!--      </div>-->
-<!--      <div class="columna-8">-->
-<!--        <a class="tarjeta tarjeta-hipervinculo-externo" href="/como-empezar" target="_self" rel="noopener noreferrer">-->
-<!--          <img class="tarjeta-imagen" src="https://cdn.conahcyt.mx/sisdai/recursos/imagenes/documentacion/asha.jpg" alt="" />-->
-<!--          <div class="tarjeta-degradado">-->
-<!--            <div class="tarjeta-cuerpo">-->
-<!--              <p class="h4">¿Cómo empezar?</p>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </a>-->
-<!--      </div>-->
-<!--      <div class="columna-8">-->
-<!--        <a class="tarjeta tarjeta-hipervinculo-externo" href="/como-colaborar" target="_self" rel="noopener noreferrer">-->
-<!--          <img class="tarjeta-imagen" src="https://cdn.conahcyt.mx/sisdai/recursos/imagenes/documentacion/kale-1.jpg" alt="" />-->
-<!--          <div class="tarjeta-degradado">-->
-<!--            <div class="tarjeta-cuerpo">-->
-<!--              <p class="h4">¿Cómo colaborar?</p>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </a>-->
-<!--      </div>-->
-
-<!--    </div>-->
   </div>
 
-  <div class="contenedor ancho-fijo p-b-maximo">
+    <div class="contenedor ancho-fijo p-b-maximo">
     <div class="flex">
         <div class="columna-8">
           <a 
@@ -198,8 +165,44 @@
             </div>
           </a>
         </div>
-
       </div>
+
+<!--    <div class="flex flex-contenido-centrado m-y-5">-->
+<!--      <div class="columna-16">-->
+<!--        <a class="tarjeta tarjeta-hipervinculo-externo" href="https://codigo.conahcyt.mx/sisdai/sisdai-css" target="_blank" rel="noopener noreferrer">-->
+<!--          <img class="tarjeta-imagen" src="https://cdn.conacyt.mx/sisdai/recursos/imagenes/documentacion/gitlab.png" alt="" />-->
+<!--          <div class="tarjeta-degradado">-->
+<!--            <div class="tarjeta-cuerpo">-->
+<!--              <p class="h4">Ir al repositorio sisdai-css <span class="pictograma-flecha-arriba-derecha"></span></p>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </a>-->
+<!--      </div>-->
+<!--      <div class="columna-8">-->
+<!--        <a class="tarjeta tarjeta-hipervinculo-externo" href="/como-empezar" target="_self" rel="noopener noreferrer">-->
+<!--          <img class="tarjeta-imagen" src="https://cdn.conahcyt.mx/sisdai/recursos/imagenes/documentacion/asha.jpg" alt="" />-->
+<!--          <div class="tarjeta-degradado">-->
+<!--            <div class="tarjeta-cuerpo">-->
+<!--              <p class="h4">¿Cómo empezar?</p>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </a>-->
+<!--      </div>-->
+<!--      <div class="columna-8">-->
+<!--        <a class="tarjeta tarjeta-hipervinculo-externo" href="/como-colaborar" target="_self" rel="noopener noreferrer">-->
+<!--          <img class="tarjeta-imagen" src="https://cdn.conahcyt.mx/sisdai/recursos/imagenes/documentacion/kale-1.jpg" alt="" />-->
+<!--          <div class="tarjeta-degradado">-->
+<!--            <div class="tarjeta-cuerpo">-->
+<!--              <p class="h4">¿Cómo colaborar?</p>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </a>-->
+<!--      </div>-->
+
+<!--    </div>-->
+  </div>
+
+  
 
 
   </div>
